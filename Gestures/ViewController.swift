01bd4ebedc413f7c0d5a2aba456359714a4bad32
Amr Salman <iamrsalman@gmail.com>
2017-09-05 12:37:48 +0200

    Initial Commit

---
+++ Gestures/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gestures
+//
+//  Created by Amr Salman on 9/5/17.
+//  Copyright © 2017 Amr Salman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
