579ccb2b292b53196d0156360bd73dc4fc40c847
Amr Salman <iamrsalman@gmail.com>
2017-09-05 15:23:14 +0200

    All changes

--- Gestures/ViewController.swift
+++ Gestures/ViewController.swift
@@ -8,18 +8,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
+  @IBAction func onAddViewBtnPressed(_ sender: Any) {
+    let newView = UIView(frame: CGRect(origin: CGPoint(x: self.view.center.x - 150, y: self.view.center.y - 150), size: CGSize(width: 150, height: 150)))
+    
+    newView.backgroundColor = getRandomColor()
+    
+    self.view.addSubview(newView)
 
+    let panGesture = UIPanGestureRecognizer(target: self, action: #selector(ViewController.handlePan(_:)))
+    panGesture.delegate = self
+    let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(ViewController.handlePinsh(_:)))
+    pinchGesture.delegate = self
+    let rotationGesture = UIRotationGestureRecognizer(target: self, action: #selector(ViewController.handelRotate(_:)))
+    rotationGesture.delegate = self
+    
+    newView.addGestureRecognizer(panGesture)
+    newView.addGestureRecognizer(pinchGesture)
+    newView.addGestureRecognizer(rotationGesture)
+
+  }
+  
+  @IBAction func handlePan(_ sender: UIPanGestureRecognizer) {
+    guard let gestureView = sender.view else { return }
+    if sender.state == .began || sender.state == .changed {
+      self.view.bringSubview(toFront: gestureView)
+      let translation = sender.translation(in: self.view)
+      gestureView.center = CGPoint(x: gestureView.center.x + translation.x, y: gestureView.center.y + translation.y)
+      sender.setTranslation(CGPoint.zero, in: self.view)
+    }
+  }
+  
+  @IBAction func handlePinsh(_ sender: UIPinchGestureRecognizer) {
+    guard let gestureView = sender.view else { return }
+    self.view.bringSubview(toFront: gestureView)
+    gestureView.transform = gestureView.transform.scaledBy(x: sender.scale, y: sender.scale)
+    sender.scale = 1.0
+  }
+  
+  @IBAction func handelRotate(_ sender: UIRotationGestureRecognizer) {
+    guard let gestureView = sender.view else { return }
+    self.view.bringSubview(toFront: gestureView)
+    gestureView.transform = gestureView.transform.rotated(by: sender.rotation)
+    sender.rotation = 0.0
+  }
+  
+  func getRandomColor() -> UIColor {
+    
+    let randomRed = CGFloat(drand48())
+    let randomGreen = CGFloat(drand48())
+    let randomBlue = CGFloat(drand48())
+    
+    return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+    
+  }
 
 }
 
